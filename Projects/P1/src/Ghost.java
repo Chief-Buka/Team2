bf9e2db8a4384024b6431532fd817fcb2d4cdc3d
eshifflett <ethan.shiffy@gmail.com>
2022-01-09 15:52:27 -0500

    Initial implementation of get_valid_moves()

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -12,8 +12,41 @@ public class Ghost{
 		this.myMap = map;
 	}
 
+	/* returns ArrayList<Location> of currently valid moves for the Ghost */ 
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		ArrayList<Location> toReturn = new ArrayList<Location>(); // reference to ArrayList to be returned
+		HashSet<Map.Type> inhabitants; // will be used to reference the inhabitants of a location, to check if valid move
+		Location curr; // will be used to reference the current location being checked
+
+		/* Up */ 
+		curr = myLoc.shift(0, 1); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Down */ 
+		curr = myLoc.shift(0, -1); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Right */ 
+		curr = myLoc.shift(1, 0); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Left */ 
+		curr = myLoc.shift(-1, 0); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}		
+
+		return toReturn;
 	}
 
 	public boolean move() {
