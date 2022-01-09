64e5b6d752363e9614714e1e39193da3d7729316
eshifflett <ethan.shiffy@gmail.com>
2022-01-09 17:59:03 -0500

    Added simple test for get_valid_moves() in PacMan.java

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		assertTrue(true);
+		NoFrame frame = new NoFrame();
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		ArrayList<Location> correct = new ArrayList<Location>();
+		correct.add(new Location(1,2));
+		correct.add(new Location(2,1));
+		
+		System.out.println("\nArray: " + pacman.get_valid_moves().toString());
+		assertTrue(correct.equals(pacman.get_valid_moves().toString()));
 	}
 }
