5787218e35084fa7a50e1955570161098094b7c3
eshifflett <ethan.shiffy@gmail.com>
2022-01-09 18:03:13 -0500

    Added simple test for get_valid_moves() in Ghost.java

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(1, 1), "testGhost", Color.BLUE);
+		ArrayList<Location> correct = new ArrayList<Location>();
+		correct.add(new Location(1,2));
+		correct.add(new Location(2,1));
+		
+		System.out.println("\nArray: " + ghost.get_valid_moves().toString());
+		assertTrue(correct.equals(ghost.get_valid_moves().toString()));
 	}
 }
