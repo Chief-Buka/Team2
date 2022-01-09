3ded4fb370822d6f04ba51bfd8f5d9d9ed95c8e4
eshifflett <ethan.shiffy@gmail.com>
2022-01-09 15:42:45 -0500

    Initial implementation of get_valid_moves() for PacMan

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -14,8 +14,41 @@ public class PacMan{
 		this.myMap = map;
 	}
 
+	/* Returns an ArrayList<Location> of currently valid moves for PacMan */
 	public ArrayList<Location> get_valid_moves() {
-		return new ArrayList<Location>;	
+		ArrayList<Location> toReturn = new ArrayList<Location>(); // reference to ArrayList to be returned
+		HashSet<Map.Type> inhabitants; // will be used to reference the inhabitants of a location, to check if valid move
+		Location curr; // will be used to reference the current location being checked
+
+		/* Up */ 
+		curr = myLoc.shift(0, 1); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Down */ 
+		curr = myLoc.shift(0, -1); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Right */ 
+		curr = myLoc.shift(1, 0); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}
+
+		/* Left */ 
+		curr = myLoc.shift(-1, 0); // sets curr
+		inhabitants = myMap.getLoc(curr); // retrieves Types on Location
+		if(!(inhabitants.contains(Map.Type.WALL))){ // Checks if there's a wall
+			toReturn.add(curr); // Adds to toReturn if no wall
+		}		
+
+		return toReturn;
 	}
 
 	public boolean move() {
