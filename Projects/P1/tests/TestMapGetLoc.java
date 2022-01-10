31dc1052e3c0556846e8f2207a09dfcd75f96a69
ejaffee01 <ejaffee01@gmail.com>
2022-01-10 17:32:43 -0500

    change

--- Projects/P1/tests/TestMapGetLoc.java
+++ Projects/P1/tests/TestMapGetLoc.java
@@ -1,16 +1,22 @@
+import static java.util.Arrays.*;
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-
+import org.junit.Assert.*;
+import org.junit.Test;
+import java.util.HashSet;
 
 public class TestMapGetLoc {
 	
-	public void testMapGetLoc() {
+	@Test
+	public void testMapGetLoc() throws FileNotFoundException {
 		NoFrame frame = new NoFrame();
 		PacMan pacman = frame.addPacMan(new Location(5, 5));
 		Map m = new Map(10);
 		Location l = new Location(5,5);
-		m.add("pac", l, new PacManComponent(5, 5, 2), Type.PACMAN);
-		assertEquals(m.getLoc(l), Type.Pacman);
+		m.add("pac", l, new PacManComponent(5, 5, 2), Map.Type.PACMAN);
+		HashSet<Map.Type> t = new HashSet<>();
+		t.add(Map.Type.PACMAN);
+		Assert.assertEquals(m.getLoc(l), t);
 	}
 }
