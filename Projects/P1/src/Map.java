4ca4db0ec2edf73fb417bde76009433804e33f55
surmud <surmud@umd.edu>
2022-01-09 20:24:42 -0500

    eat cookie completed

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -67,10 +67,43 @@ public class Map{
 		//update gameOver
 		return false;
 	}
-	
+
+	/* The method controls Pacman eating a cookie. When the function is able to successfully update display to eat a cookie it returns 
+	the Cookie component that has been eaten, otherwise it returns null.*/
 	public JComponent eatCookie(String name) {
-		//update locations, components, field, and cookies
-		//the id for a cookie at (10, 1) is tok_x10_y1
-		return null;
+		
+
+		//see if there's a cookie at pacman's location 
+		Location pacman_loc = locations.get("pacman");
+		HashSet<Type> loc = getLoc(pacman_loc);
+		if (loc.contains(Type.COOKIE)) {
+			//update locations, components, field, and cookies
+			
+			//decrement cookies
+			cookies -= 1;
+
+			//remove cookie from locations
+			String location_string = "tok_x"+ pacman_loc.x + "_y"+ pacman_loc.y;
+			locations.remove(location_string);
+
+			//access the cookie's jcomponent for later
+			JComponent cookie_to_return = components.get(location_string);
+
+			//remove the cookie from components set
+			components.remove(location_string);
+
+			//remove cookie from field set
+			loc.remove(Type.COOKIE);
+
+			return cookie_to_return;
+
+		//if there's no cookie at the specified location, return null
+		} else {
+			return null;
+		}
+		//locations: String, Location
+		//components: String, JComponent
+		//field: Location, Hashset<Type>
+		//the id for a cookie at (10, 1) is tok_x10_y1		
 	}
 }
