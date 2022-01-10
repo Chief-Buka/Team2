d6a9c29a05909c24a8d30aaafe41119cd467b7c9
ejaffee01 <ejaffee01@gmail.com>
2022-01-10 13:19:45 -0500

    fixed test - should pass  when merged

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -1,13 +1,16 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestGhostMove extends TestCase {
 
 	@Test
 	public void testGhostMove() throws FileNotFoundException{
-		Mainframe frame = new MainFrame();
-		Ghost ghost = frame.addPacMan(new Location(9, 11));
-		assertEquals(true, pacman.move());
+		NoFrame frame = new NoFrame();
+		// Map m = new Map(20);
+		Ghost ghost = frame.addGhost(new Location(9, 11), "NewGhost", Color.red);
+		assertEquals(true, ghost.move());
 	}
 }
