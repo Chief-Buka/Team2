4ec5d1ca8a269026e28d736c2ae07b0843fc30fa
ejaffee01 <ejaffee01@gmail.com>
2022-01-10 20:29:03 -0500

    update tests

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -1,14 +1,17 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestPacManMove extends TestCase {
-
+	@Test
 	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		PacMan pacman = frame.addPacMan(new Location(9, 11));
+		PacMan pacman = frame.addPacMan(new Location(5, 5));
 		assertEquals(true, pacman.move());
 	}
 }
+
 
 
