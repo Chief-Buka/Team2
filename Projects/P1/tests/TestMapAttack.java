b6f878fdce942376bd2118edc195a312b68b4f3f
Michael Brown <mikeb196840@gmail.com>
2022-01-15 22:42:43 -0500

    Sabotaged

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -6,8 +6,8 @@ public class TestMapAttack extends TestCase {
 
 	public void testMapAttack() throws FileNotFoundException{
 		//Creating A Map
-		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-		//NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+		//MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
 		//Creating Players
 		Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
